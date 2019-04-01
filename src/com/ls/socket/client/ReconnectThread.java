3115f4870519dd4b72987364ea05f068b264dced
yingqingFan <732722340@qq.com>
2019-04-01 18:26:25 +0800

    [feature]add function client reconnect

---
+++ src/com/ls/socket/client/ReconnectThread.java
@@ -0,0 +1,70 @@
+package com.ls.socket.client;
+
+import com.google.gson.Gson;
+import com.ls.socket.entity.MessageInfo;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ReconnectThread extends Thread{
+    private Socket socket;
+    private String ip;
+    private int port;
+    public ReconnectThread(Socket socket,String ip, int port) {
+        this.socket = socket;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            sendHeartBeat();
+        }
+
+    }
+
+    public void sendHeartBeat(){
+        PrintStream printStream = null;
+        if(socket != null){
+            try {
+                printStream = new PrintStream(socket.getOutputStream());
+                MessageInfo messageInfo = new MessageInfo();
+                messageInfo.setClientId(SocketClient.clientId);
+                messageInfo.setAction(SocketClient.ACTIONS[4]);
+                messageInfo.setMessageContent("心跳");
+                printStream.println(new Gson().toJson(messageInfo));
+                printStream.flush();
+            } catch (IOException e) {
+                try {
+                    socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                if(printStream!=null) {
+                    printStream.close();
+                }
+                reconnect(ip, port);
+            }
+        }else{
+            reconnect(ip, port);
+        }
+    }
+
+    public void reconnect(String ip, int port){
+        System.out.println("尝试重新连接...");
+        socket = SocketClient.initClient(ip, port);
+        if(socket!=null){
+            //接收消息
+            SocketClient.receiveMessage(socket);
+            //发送消息
+            SocketClient.sendMessage(socket);
+        }
+    }
+}
